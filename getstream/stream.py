a8f33829e9f70c730ebd3b6a7e6e3a93b8219eed
fealuin <fealuin@gmail.com>
2014-10-22 17:35:23 +0000

    Ya cargando en BD

--- getstream/stream.py
+++ getstream/stream.py
@@ -2,90 +2,58 @@ import time
 from secret import *
 from getpass import getpass
 from textwrap import TextWrapper
-
 import tweepy
+import MySQLdb
+
+def addToDb(status):
+    add_user=("INSERT INTO `USERS` (`ID_USER`,`NAME`,`TIMEZONE`,`LANGUAGE`,`FOLLOWERS`,`DESCRIPTION`) VALUES (%s,%s,%s,%s,%s,%s)")
+    data_user=(status.user.id_str,status.user.name,status.user.time_zone,status.user.lang,status.user.followers_count,status.user.description)
+
+    add_tweet=("INSERT INTO `TWEETS` (`TEXT`,`CREATED_AT`,`RETWEETS`,`ID_USER`,`LOCATION`,`SOURCE`,`COORDINATES`,`GEOENABLE`,`ID_TWITTER`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+    data_tweet=(status.text.econde('utf8'),status.created_at,status.retweet_count,status.user.id_str,status.user.location,status.source,status.coordinates.encode('utf8'),status.user.geo_enabled,status.id_str)
+
+    add_incident=("INSERT INTO `INCIDENTS` (`ID_TWEET`,`ID_ITYPE`) VALUES (%s,%s)")
+    data_incident=(1,2)
+    cursor.execute(add_user,data_user)
+    cursor.execute(add_tweet,data_tweet)
+    db.commit()
+    cursor.close()
+    db.close()
 
 
 class StreamWatcherListener(tweepy.StreamListener):
-
     status_wrapper = TextWrapper(width=60, initial_indent='    ', subsequent_indent='    ')
-
     def on_status(self, status):
         try:
-            print self.status_wrapper.fill(status.text)
-            print '\n %s  %s  via %s\n' % (status.author.screen_name, status.created_at, status.source)
+            db2 = MySQLdb.connect(host,user,passwd, db)
+            cursor=db2.cursor()
+            db2.set_character_set('utf8')
+            cursor.execute('SET NAMES utf8;') 
+            cursor.execute('SET CHARACTER SET utf8;')
+            cursor.execute('SET character_set_connection=utf8;')
+#            print(status.coordinates)           
+#            print(status.coordinates[u'coordinates'])
+            if status.coordinates:
+                coor=status.coordinates[u'coordinates']
+            else:
+                coor=""
 
-            print "Tweet Text: ",status.text
+            add_user=("INSERT INTO `USERS` (`ID_USER`,`NAME`,`TIMEZONE`,`LANGUAGE`,`FOLLOWERS`,`DESCRIPTION`) VALUES (%s,%s,%s,%s,%s,%s)")
+            data_user=(status.user.id_str,status.user.name,status.user.time_zone,status.user.lang,status.user.followers_count,status.user.description)
 
-            text = status.text
-
-            print "Time Stamp: ",status.created_at
-
-            print "Time Stamp: ",status.created_at
-
-            print "Source: ",status.source
-
-            source = status.source
-
-            print "Author: ",status.user.screen_name
-
-            author = status.user.screen_name
-
-            print "Name: ",status.user.name
-
-            name = status.user.name
-
-            print "Time Zone: ",status.user.time_zone
-
-            time_zone = status.user.time_zone
-
-            print "User Language: ",status.user.lang
-
-            user_language = status.user.lang
-
-            print "Followers: ",status.user.followers_count
-
-            followers = status.user.followers_count
-
-            print "User Description: ",status.user.description
-
-            user_description = status.user.description
-
-            print "Geo Enabled: ",status.user.geo_enabled
-
-            geo_enabled = status.user.geo_enabled
-
-            print "Friends: ",status.user.friends_count
-
-            friends = status.user.friends_count
-
-            print "Retweets: ",status.retweet_count
-
-            retweets = status.retweet_count
-
-            print "Location: ",status.user.location
-
-            location = status.user.location
-
-            print "ID: ",status.user.id_str
-
-            user_id = status.user.id_str
-
-            print "Coordinates: ",status.coordinates
-
-            coordinates = status.coordinates
-
-            print "Place: ",status.place
-
-            place = status.place
+            add_tweet=("INSERT INTO `TWEETS` (`TEXT`,`CREATED_AT`,`RETWEETS`,`ID_USER`,`LOCATION`,`SOURCE`,`COORDINATES`,`GEOENABLE`,`ID_TWITTER`) VALUES (%s,%s,%s,%s,%s,%s,\"%s\",%s,%s)")
+            data_tweet=(status.text,status.created_at,status.retweet_count,status.user.id_str,status.user.location,status.source,coor,status.user.geo_enabled,status.id_str)
+#            print(add_tweet,data_tweet)
+            add_incident=("INSERT INTO `INCIDENTS` (`ID_TWEET`,`ID_ITYPE`) VALUES (%s,%s)")
+            data_incident=(1,2)
+            cursor.execute(add_user,data_user)
+            cursor.execute(add_tweet,data_tweet)
+            db2.commit()
+            cursor.close()
+            db2.close()
 
 
-
-
-
-
-
-        except:
+        except Exception as e:
             # Catch any unicode errors while printing to console
             # and just ignore them to avoid breaking application.
             pass
@@ -111,48 +79,16 @@ def main():
     auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = tweepy.Stream(auth, StreamWatcherListener(), timeout=None)
-
+    i=0
     # Prompt for mode of streaming
     valid_modes = ['sample', 'filter']
     while True:
-        mode = raw_input('Mode? [sample/filter] ')
-        if mode in valid_modes:
-            break
-        print 'Invalid mode! Try again.'
 
-    if mode == 'sample':
  #       stream.sample()
 #        stream.filter(locations=[-33.095480,-71.809518,-34.108874,-69.859445])
-        stream.filter(locations=[-71.288477,-33.960067,-70.090967,-33.050879])
-    elif mode == 'filter':
-        follow_list = raw_input('Users to follow (comma separated): ').strip()
-        track_list = raw_input('Keywords to track (comma seperated): ').strip()
-        if follow_list:
-            follow_list = [u for u in follow_list.split(',')]
-            userid_list = []
-            username_list = []
-            
-            for user in follow_list:
-                if user.isdigit():
-                    userid_list.append(user)
-                else:
-                    username_list.append(user)
-            
-            for username in username_list:
-                user = tweepy.API().get_user(username)
-                userid_list.append(user.id)
-            
-            follow_list = userid_list
-        else:
-            follow_list = None
-        if track_list:
-            track_list = [k for k in track_list.split(',')]
-        else:
-            track_list = None
-        print follow_list
-        stream.filter(follow_list, track_list)
-
-
+         stream.filter(locations=[-71.288477,-33.960067,-70.090967,-33.050879])
+         print i
+         i=i+1
 if __name__ == '__main__':
     try:
         main()
--- getstream/testdb.py
+++ getstream/testdb.py
@@ -1,26 +1,53 @@
 from secret import *
 import MySQLdb
+import time
 
 db = MySQLdb.connect(host,user,passwd, db)
 cursor=db.cursor()
 add_test=("INSERT INTO TEST (ID) VALUES (%s)")
 data=("1")
 
-add_tweet=("INSERT INTO `TWEETS` (`TEXT`,`CREATED_AT`,`RETWEETS`,`ID_USER`,`LOCATION`,`SOURCE`,`COORDINATES`,`PLACE`,`GEOENABLE`,`ID_TWITTER`,`new field`) VALUES ('','','%s','%s','%s','%s','%s','%s','%s','%s','%s')")
-add_user=("INSERT INTO `USERS` (`ID_USER`,`NAME`,`TIMEZONE`,`LANGUAGE`,`FOLLOWERS`,`DESCRIPTION`) VALUES ('%s','%s','%s','%s','%s','%s')")
+add_user=("INSERT INTO `USERS` (`ID_USER`,`NAME`,`TIMEZONE`,`LANGUAGE`,`FOLLOWERS`,`DESCRIPTION`) VALUES (%s,%s,%s,%s,%s,%s)")
+data_user=('1','Juan','CL','ES',100,'hola soy juam y esta es mi descripcion')
 
-add_incident("INSERT INTO `INCIDENTS` (`ID_INCIDENT`,`ID_TWEET`,`ID_ITYPE`) VALUES ('','','')")
+add_tweet=("INSERT INTO `TWEETS` (`TEXT`,`CREATED_AT`,`RETWEETS`,`ID_USER`,`LOCATION`,`SOURCE`,`COORDINATES`,`PLACE`,`GEOENABLE`,`ID_TWITTER`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)")
+data_tweet=('HOLA ESTE ES UN TWEET',time.time(),10,'1','3e12321,312321','android','stgo','aqui',1,'31231232131')
+
+add_incident=("INSERT INTO `INCIDENTS` (`ID_TWEET`,`ID_ITYPE`) VALUES (%s,%s)")
+data_incident=(1,2)
+
+#def test():
+try:
+    cursor.execute(add_user,data_user)
+except:
+    pass
+
+try:
+   cursor.execute(add_tweet,data_tweet)
+except:
+    pass
+
+cursor.execute(add_incident,data_incident)
 
 
 
-for x in range(0,100):
+cursor.execute("select * from USERS")
 
-	cursor.execute(add_test,x)
+for reg in cursor:
+    print(reg)
 
-cursor.execute("SELECT * FROM TEST")
+cursor.execute("select * from TWEETS")
 
-for RID,ID in cursor:
-    print(RID,ID)
+for reg in cursor:
+    print(reg)
+
+cursor.execute("select * from INCIDENTS")
+
+for reg in cursor:
+    print(reg)
+
+
+
 db.commit()
 cursor.close()
 db.close()
